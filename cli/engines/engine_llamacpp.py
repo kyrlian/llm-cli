8ea61b940ee45033dba32f82caa85012180d290f
Rémi Serra <remi.serra@fr.ibm.com>
2024-05-11 19:23:37 +0200

    llamacpp is borken

---
+++ cli/engines/engine_llamacpp.py
@@ -0,0 +1,37 @@
+from huggingface_hub import hf_hub_download
+from langchain_community.llms import LlamaCpp
+
+MODELS_PATH = "./models/"
+
+
+class Engine:
+
+    def __init__(self):
+        # https://github.com/PromtEngineer/localGPT/blob/main/load_models.py
+        # https://python.langchain.com/docs/integrations/llms/llamacpp
+        model_id = "TheBloke/TinyLlama-1.1B-python-v0.1-GGUF"
+        model_basename = "tinyllama-1.1b-python-v0.1.Q5_K_M.gguf"
+        model_path = hf_hub_download(
+            repo_id=model_id,
+            filename=model_basename,
+            resume_download=True,
+            cache_dir=MODELS_PATH,
+        )
+        kwargs = {
+            "model_path": model_path,
+            "n_ctx": 4096,
+            "max_tokens": 4096,
+            "n_batch": 512,  # set this based on your GPU & CPU RAM
+            "n_gpu_layers": 100,  # Llama-2-70B has 83 layers
+        }
+        self.llm = LlamaCpp(**kwargs)
+
+    def generate(self, input):
+        generated_text = self.llm(input)
+        # print(f"DEBUG:local_engine:generate:generated_text: '{generated_text}'")
+        return generated_text
+
+
+if __name__ == "__main__":
+    en = Engine()
+    print(en.generate("Who is obama?"))
