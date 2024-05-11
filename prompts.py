133ca0c7da1f99d370b3a75f142b7a22cbaddfbd
Rémi Serra <remi.serra@fr.ibm.com>
2024-05-11 18:11:32 +0200

    improve prompts

--- prompts.py
+++ prompts.py
@@ -1,12 +1,12 @@
 prompts = {
     "en": {
-        "summarize": "Please summarize the following TEXT in one or two sentences, do no add comments:\n\nTEXT\n\n{payload}\n",
-        "translate": "Please translate the following TEXT to French, do no add comments:\n\nTEXT\n\n{payload}\n",
-        "extend": "Please extend the following BULLET LIST in a few paragraphs, do no add comments:\n\BULLET LIST\n\n{payload}\n",
+        "summarize": "Please summarize the text beetween ==START TEXT== and ==END TEXT== in one or two sentences, do no add comments:\n\n==START TEXT==\n\n{payload}\n\n==END TEXT==\n",
+        "translate": "Please translate the text beetween ==START TEXT== and ==END TEXT== to French, do no add comments:\n\n==START TEXT==\n\n{payload}\n\n==END TEXT==\n",
+        "extend": "Please extend the bullet list beetween ==START TEXT== and ==END TEXT== in a few paragraphs, do no add comments:\n\n==START TEXT==\n\n{payload}\n\n==END TEXT==\n",
     },
     "fr": {
-        "summarize": "Veuillez résumer le TEXTE suivant en une ou deux phrases, sans ajouter de commentaires :\n\nTEXTE\n\n{payload}\n",
-        "translate": "Veuillez traduire le texte suivant en Anglais, ne pas ajouter de commentaires :\n\nTEXT\n\n{payload}\n",
-        "extend": "Veuillez détailler la LISTE suivante sous forme de quelques paragraphes :\n\nLISTE\n\n{payload}\n",
+        "summarize": "Veuillez résumer le texte entre ==DEBUT TEXTE== et ==FIN TEXTE== en une ou deux phrases, sans ajouter de commentaires :\n\n==DEBUT TEXTE==\n\n{payload}\n\n==FIN TEXTE==\n",
+        "translate": "Veuillez traduire le texte entre ==DEBUT TEXTE== et ==FIN TEXTE== en Anglais, ne pas ajouter de commentaires :\n\n==DEBUT TEXTE==\n\n{payload}\n\n==FIN TEXTE==\n",
+        "extend": "Veuillez détailler la liste entre ==DEBUT TEXTE== et ==FIN TEXTE== sous forme de quelques paragraphes :\n\n==DEBUT TEXTE==\n\n{payload}\n\n==FIN TEXTE==\n",
     },
 }
