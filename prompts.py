bdd869a4e434cf97f13a30520ffad7bae3479b9f
Rémi Serra <remi.serra@fr.ibm.com>
2024-05-11 18:04:54 +0200

    add prompt samples

--- prompts.py
+++ prompts.py
@@ -1,10 +1,12 @@
 prompts = {
     "en": {
-        "summarize": "Please summarize the following TEXT in one or two sentences, do no add comments:\n\nTEXT\n\n{tmptext}\n",
-        "suggest": "Please read the STORY SO FAR below, and the provided INSTRUCTIONS, then please complete the story following the provided instructions. Do not add comments, only print the summary.\n\nSTORY SO FAR\n\n{fullsummary}\n\nINSTRUCTIONS\n\n{instructions}\n",
+        "summarize": "Please summarize the following TEXT in one or two sentences, do no add comments:\n\nTEXT\n\n{payload}\n",
+        "translate": "Please translate the following TEXT to French, do no add comments:\n\nTEXT\n\n{payload}\n",
+        "extend": "Please extend the following BULLET LIST in a few paragraphs, do no add comments:\n\BULLET LIST\n\n{payload}\n",
     },
     "fr": {
-        "summarize": "Veuillez résumer le TEXTE suivant en une ou deux phrases, sans ajouter de commentaires :\n\nTEXTE\n\n{tmptext}\n",
-        "suggest": "Veuillez lire l'HISTOIRE JUSQUE ICI ci-dessous et les INSTRUCTIONS fournies, puis complétez l'histoire en suivant les instructions fournies. N'ajoutez pas de commentaires, affichez seulement le résumé.\n\nHISTOIRE JUSQUE ICI\n\n{fullsummary}\n\nINSTRUCTIONS\n\n{instructions}\n",
+        "summarize": "Veuillez résumer le TEXTE suivant en une ou deux phrases, sans ajouter de commentaires :\n\nTEXTE\n\n{payload}\n",
+        "translate": "Veuillez traduire le texte suivant en Anglais, ne pas ajouter de commentaires :\n\nTEXT\n\n{payload}\n",
+        "extend": "Veuillez détailler la LISTE suivante sous forme de quelques paragraphes :\n\nLISTE\n\n{payload}\n",
     },
 }
