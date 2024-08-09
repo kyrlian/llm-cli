ef33fbe19504a62bf9b140d363d16c92672a2e3d
kyrlian <kyrlian@gmail.com>
2024-08-09 13:46:08 +0200

    additional args

--- cli/engines/engine_ollama.py
+++ cli/engines/engine_ollama.py
@@ -4,10 +4,11 @@ import requests
 
 
 class Engine:
-    def __init__(self, model_id="llama3", port=11434):
+    def __init__(self, model_id="llama3", port=11434, verbose=False):
         self.model = model_id
         self.url = f"http://localhost:{port}/api"
         self.headers = {"Content-Type": "application/json"}
+        self.verbose = verbose
 
     def list(self):
         response = requests.get(f"{self.url}/tags", headers=self.headers)
@@ -18,18 +19,33 @@ class Engine:
             return modelnames
         else:
             print("Error:", response.status_code, response.text)
+            return []
 
-    def generate(self, prompt, model=None):
+
+    def generate_response(self, prompt, model=None, temperature=None, max_tokens=None, stop=[]):
         if model is None:
             model = self.model
-        data = {"model": model, "stream": False, "prompt": prompt}
+        options={"temperature":temperature, "num_predict":max_tokens, "stop":stop}
+        data = {"model": model, "stream": False, "prompt": prompt, "options":options}
         response = requests.post(f"{self.url}/generate", headers=self.headers, json=data)
         if response.status_code == 200:
             response_json = response.json()
-            actual_response = response_json["response"]
-            return actual_response
+            if self.verbose:
+                print("Response JSON:", response_json)
+            return response_json
         else:
             print("Error:", response.status_code, response.text)
+            return None
+
+    def generate_text(self, prompt, model=None, temperature=None, max_tokens=None, stop=[]):
+        response_json = self.generate_response(prompt, model, temperature, max_tokens, stop)
+        if response_json is not None:
+            actual_response = response_json["response"]
+            return actual_response
+        return None
+    
+    def generate(self, prompt, model=None, temperature=None, max_tokens=None, stop=[]):
+        return self.generate_text(prompt, model, temperature, max_tokens, stop)
 
 
 if __name__ == "__main__":
